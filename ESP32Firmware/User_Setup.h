c235b93585c6933e49fa028f93ff2255db257548
Nguyễn Minh Hoàng <69524738+HonagF@users.noreply.github.com>
2026-04-08 22:03:33 +0700

    Add files via upload

---
+++ ESP32Firmware/User_Setup.h
@@ -0,0 +1,13 @@
+#define ILI9488_DRIVER     // Driver cho màn 3.5 inch
+#define TFT_BL   32        // Nếu muốn điều khiển độ sáng bằng GPIO 32 (tùy chọn)
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS    5
+#define TFT_DC    2
+#define TFT_RST   4
+#define LOAD_GLCD  
+#define LOAD_FONT2 
+#define LOAD_FONT4 
+#define LOAD_FONT7 
+#define SPI_FREQUENCY  40000000 // Chạy ở 40MHz cho mượt
